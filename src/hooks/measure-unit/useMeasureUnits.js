fbea2d7cfbd8157b31a5045cdfb1057b63a706ca
Alejandro Perez <aperez@velosimo.com>
2022-04-27 00:55:06 -0400

    Fix problem for eliminate ,Add Notification for edit, insert , eliminated, change name form for edit o eliminated and agregate function for eliminated

--- src/hooks/measure-unit/useMeasureUnits.js
+++ src/hooks/measure-unit/useMeasureUnits.js
@@ -1,6 +1,6 @@
-import { useQuery } from 'react-query';
 import { apiFetcher } from '@/lib/apiFetcher';
-import { API_MEASURE_UNITS_URL, POST } from '@/lib/constants';
+import { API_MEASURE_UNITS_URL, DELETE, POST, PUT } from '@/lib/constants';
+import { useQuery } from 'react-query';
 
 const getMeasureUnits = async ({ queryKey }) => {
   const [path, params] = queryKey;
@@ -8,18 +8,32 @@ const getMeasureUnits = async ({ queryKey }) => {
   return data;
 };
 
-const createMeasureUnit = async (args) => {
+const safeMeasureUnit = async (args) => {
   const { path, data: values, method } = args;
   const { data } = await apiFetcher(path, { data: values, method });
   return data;
 };
 
+const deleteMeasureUnit = async (args) => {
+  const { path, method } = args;
+  const data = await apiFetcher(path, { method });
+  return data;
+};
+
 export default function useMeasureUnits({ args = {}, options = {} } = {}) {
-  if (options?.method === POST) {
-    createMeasureUnit({ path: API_MEASURE_UNITS_URL, data: args, method: POST });
-  } else {
-    return useQuery([API_MEASURE_UNITS_URL, { ...args }], getMeasureUnits, {
-      ...options
-    });
+  switch (options?.method) {
+    case POST:
+      safeMeasureUnit({ path: API_MEASURE_UNITS_URL, data: args, method: POST });
+      break;
+    case DELETE:
+      deleteMeasureUnit({ path: API_MEASURE_UNITS_URL + `/${args.id}`, method: DELETE });
+      break;
+    case PUT:
+      safeMeasureUnit({ path: API_MEASURE_UNITS_URL + `/${args.id}`, data: args, method: PUT });
+      break;
+    default:
+      return useQuery([API_MEASURE_UNITS_URL, { ...args }], getMeasureUnits, {
+        ...options
+      });
   }
 }
