099a373f1ee3f4bf596896ef24c4b376415be154
Alejandro Perez <aperez@velosimo.com>
2022-07-01 17:04:18 -0400

    Add option view details to action table
     Changes to be committed:
    	modified:   src/components/table/TableActions.jsx

--- src/components/table/TableActions.jsx
+++ src/components/table/TableActions.jsx
@@ -1,8 +1,8 @@
-import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
+/* eslint-disable react/react-in-jsx-scope */
+import { EyeIcon, PencilIcon, TrashIcon } from '@heroicons/react/outline';
 import PropTypes from 'prop-types';
-import React from 'react';
 
-const TableActions = ({ onEdit, onDelete }) => {
+const TableActions = ({ onEdit, onDelete, onViewDetails }) => {
   return (
     <div className="z-40 flex items-center justify-end space-x-4">
       {onEdit ? (
@@ -25,13 +25,24 @@ const TableActions = ({ onEdit, onDelete }) => {
           <TrashIcon className="w-6 h-6" />
         </button>
       ) : null}
+      {onViewDetails ? (
+        <button
+          type="button"
+          className="p-1 rounded-full hover:bg-red-100 hover:text-red-500"
+          id="buttonDelete"
+          onClick={onViewDetails}
+        >
+          <EyeIcon className="w-6 h-6" />
+        </button>
+      ) : null}
     </div>
   );
 };
 
 TableActions.propTypes = {
   onEdit: PropTypes.func,
-  onDelete: PropTypes.func
+  onDelete: PropTypes.func,
+  onViewDetails: PropTypes.func
 };
 
 export default TableActions;
