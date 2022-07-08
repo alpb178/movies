6432d40fffd6bc2cc19a559abce949a7daa4eb26
Alejandro Perez <aperez@velosimo.com>
2022-07-08 15:53:06 -0400

    Adjust properties z-index and z for component loader

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/react-in-jsx-scope */
+import PropTypes from 'prop-types';
 import Lottie from 'react-lottie';
 import animationData from '/public/lottie/loading-spinner.json';
 
@@ -13,10 +14,15 @@ const defaultOptions = {
 
 export default function Loader({ height, width }) {
   return (
-    <div className="absolute inset-0 bg-white  bg-opacity-75 z-index-2 flex items-center justify-center ">
+    <div className="absolute inset-0 bg-white  bg-opacity-75 z-20 z-index-2 flex items-center justify-center ">
       <div className="h-40   w-max">
         <Lottie options={defaultOptions} height={height} width={width} />
       </div>
     </div>
   );
 }
+
+Loader.propTypes = {
+  height: PropTypes.number,
+  width: PropTypes.number
+};
