a0cdf6d434d1b77787e99af0321afdc68d7bc40d
alegd <ale.gueden@gmail.com>
2022-08-09 00:26:15 -0400

    update loading component

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,4 +1,5 @@
-/* eslint-disable react/react-in-jsx-scope */
+import clsx from 'clsx';
+import useTranslation from 'next-translate/useTranslation';
 import PropTypes from 'prop-types';
 import Lottie from 'react-lottie';
 import animationData from '/public/lottie/loading-spinner.json';
@@ -12,11 +13,19 @@ const defaultOptions = {
   }
 };
 
-export default function Loader({ height, width }) {
+export default function Loader({ height, width, fs = true }) {
+  const { t } = useTranslation('common');
+
   return (
-    <div className="absolute inset-0 bg-white  bg-opacity-75 z-50 z-index-2 flex items-center justify-center ">
-      <div className="h-40   w-max">
+    <div
+      className={clsx(
+        fs ? 'absolute inset-0' : '',
+        'bg-white z-50 flex items-center justify-center w-full'
+      )}
+    >
+      <div className="flex flex-col items-center h-56 w-max">
         <Lottie options={defaultOptions} height={height} width={width} />
+        <p className="text-lg font-medium text-gray-700">{t('loading')}</p>
       </div>
     </div>
   );
