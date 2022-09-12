926ed97048bdb6dcf625f0dc14d6fe4954aa365e
Alejandro Perez <aperez@velosimo.com>
2022-09-12 17:19:27 -0400

    Add List Categories List Menu

--- src/containers/menu-items/MenuGroups.jsx
+++ src/containers/menu-items/MenuGroups.jsx
@@ -1,51 +1,42 @@
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/display-name */
+import EmptyState from '@/components/common/EmptyState';
+import Loading from '@/components/common/Loader';
+import ContainerSidebarRight from '@/components/container/ContainerSidebarRight';
 import { useAppContext } from '@/components/context/AppContext';
-import RightSidebar from '@/components/sidebar/RightSidebar';
 import { saveProduct } from '@/hooks/product/useProducts';
+import useCategoryRecipes from '@/hooks/recipe-groups/useRecipesGroups';
 import { API_PRODUCTS_CATALOG_URL, POST, PUT } from '@/lib/constants';
+import { PencilIcon, TrashIcon } from '@heroicons/react/outline';
 import useTranslation from 'next-translate/useTranslation';
 import PropTypes from 'prop-types';
 import { useEffect, useState } from 'react';
 import { useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
-import * as Yup from 'yup';
+import ListItemActions from './ListItemActions';
 
-const MenuGroups = ({ data, onOpen, open, products, setLoading }) => {
+const MenuCategories = ({ data, onOpen, open, setLoading }) => {
   const { t } = useTranslation('common');
   const queryClient = useQueryClient();
   const [isNewData, setIsNewData] = useState(true);
-  const [errors, setErrorsForm] = useState({});
-  const [touched, setTouchedForm] = useState({});
-  const [directSale, setDirectSale] = useState(false);
 
   const { user } = useAppContext();
 
-  const initialValues = {
-    name: data?.name || '',
-    measureUnit: data?.measureUnit || ''
-  };
-
-  // const { data: recipeGroups } = useRecipeGroups({
-  //   args: {},
-  //   options: {
-  //     keepPreviousData: true
-  //   }
-  // });
-
-  const validationSchema = Yup.object().shape({
-    name: Yup.string().required(t('form.common.required.name'))
+  const { data: recipesGroups, isloading } = useCategoryRecipes({
+    args: {},
+    options: {
+      keepPreviousData: true
+    }
   });
 
   const onSubmit = async (values) => {
-    const { name } = values;
-    const body = { name };
+    const { name, price, directSale, cost, description, measureUnit } = values;
+    const sendBody = { name, price, directSale, cost, description };
+    sendBody.measureUnit = measureUnit.id;
     let method = POST;
-    let message = t('inserted.male', { entity: t('recipe-group', { count: 1 }) });
+    let message = t('inserted.male', { entity: t('products', { count: 1 }) });
     if (data) {
       method = PUT;
       sendBody.id = data.id;
-      message = t('updated.male', { entity: t('recipe-group', { count: 1 }) });
+      message = t('updated.male', { entity: t('products', { count: 1 }) });
     }
 
     try {
@@ -66,6 +57,12 @@ const MenuGroups = ({ data, onOpen, open, products, setLoading }) => {
     }
   };
 
+  const renderInsertButton = () => (
+    <button type="button" className="btn-contained" onClick={() => setOpenForm(true)}>
+      {t('create', { entity: t('recipe-groups', { count: 1 }).toLowerCase() })}
+    </button>
+  );
+
   useEffect(() => {
     data?.id ? setIsNewData(false) : setIsNewData(true);
   }, [data?.id]);
@@ -74,103 +71,66 @@ const MenuGroups = ({ data, onOpen, open, products, setLoading }) => {
     onOpen(true);
   });
 
+  const actions = [
+    {
+      icon: PencilIcon,
+      id: 'update',
+      name: t('update')
+    },
+    {
+      icon: TrashIcon,
+      id: 'delete',
+      name: t('delete')
+    }
+  ];
+
   return (
-    <RightSidebar open={open} onOpen={onOpen}>
-      {/* <div className="p-6 space-y-6 text-lg">
-        <div className="space-y-2">
-          <div className="relative w-full mx-auto">
-            <div className="absolute z-10 text-center text-gray-500"></div>
-            <Field
-              name="name"
-              className={clsx(
-                'text-field',
-                errors?.shipmentItems && touched?.shipmentItems
-                  ? 'border-red-400 bg-red-100'
-                  : ' filled'
-              )}
-            />
-          </div>
-        </div>
-
-        <div className="w-full space-y-2">
-          <label htmlFor="category">{t('measure-units', { count: 1 })}</label>
-          <div className="relative w-full mx-auto">
-            <AutocompleteField
-              id="measureUnit"
-              name="measureUnit"
-              options={measureUnits?.rows ? measureUnits.rows : []}
-              className="text-field filled"
-              defaultValue={data?.measureUnit}
-              placeholder={t('select')}
-            />
-          </div>
-        </div>
-
-        <div className="space-y-2">
-          <label htmlFor="cost">{t('form.common.label.cost')}</label>
-          <div className="relative w-full mx-auto">
-            <Field id="cost" type="number" name="cost" className="text-field filled" />
-            {errors?.cost && touched?.cost ? (
-              <p className="mt-4 text-red-600">{errors?.cost}</p>
-            ) : null}
-          </div>
-        </div>
-
-        <div className="space-y-2">
-          <label htmlFor="description">{t('form.common.label.description')}</label>
-          <Field
-            as="textarea"
-            className="text-field filled"
-            rows={3}
-            id="description"
-            name="description"
-          />
-        </div>
-
-        <div className="flex justify-between">
-          <label className="flex items-center">{t('direct-sale')}</label>
-          <Field id="directSale" name="directSale">
-            {({ form: { values, setFieldValue } }) => (
-              <CustomSwitch
-                checked={values?.directSale}
-                onChange={(val) => {
-                  // setPushNotification(val);
-                  setFieldValue('directSale', val);
-                  setDirectSale(!directSale);
-                }}
-              />
+    <ContainerSidebarRight
+      formName="recipe-groups"
+      open={open}
+      onOpen={onOpen}
+      isNewData={isNewData}
+    >
+      <div className="p-5">
+        {isloading && <Loading />}
+        <div className="flow-root mt-1">
+          <div>
+            {recipesGroups?.rows?.length < 0 || (
+              <div className="flex flex-col justify-between w-full space-y-1 lg:items-center lg:space-y-0 lg:space-x-12 lg:flex-row">
+                {renderInsertButton()}
+              </div>
             )}
-          </Field>
-        </div>
-
-        {directSale ? (
-          <div className="space-y-2">
-            <label htmlFor="price">{t('form.common.label.price')}</label>
-            <div className="relative w-full mx-auto">
-              <Field id="price" type="number" name="price" className="text-field filled" />
-              {errors?.price && touched?.price ? (
-                <p className="mt-4 text-red-600">{errors?.price}</p>
-              ) : null}
-            </div>
+            <ul role="list" className="mt-12 divide-y divide-gray-200">
+              {recipesGroups && recipesGroups?.length > 0 ? (
+                recipesGroups.map((recipeGroup) => (
+                  <>
+                    <li key={recipeGroup.id} className="py-3 text-base cursor-pointer">
+                      <div className="relative flex items-center justify-between text-gray-800">
+                        <p>{t(recipeGroup.name, { count: 2 })}</p>
+                        <ListItemActions
+                          actions={actions}
+                          resource={recipeGroup}
+                          setLoading={setLoading}
+                        />
+                      </div>
+                    </li>
+                  </>
+                ))
+              ) : (
+                <EmptyState className="text-sm" text={t('recipe-groups', { count: 0 })} />
+              )}
+            </ul>
           </div>
-        ) : null}
-      </div> */}
-    </RightSidebar>
+        </div>
+      </div>
+    </ContainerSidebarRight>
   );
 };
 
-MenuGroups.defaultProps = {
-  data: null,
-  errors: null
-};
-
-MenuGroups.propTypes = {
-  data: PropTypes.object,
-  errors: PropTypes.object,
+MenuCategories.propTypes = {
   onOpen: PropTypes.func.isRequired,
   open: PropTypes.bool.isRequired,
-  touched: PropTypes.object,
   setLoading: PropTypes.func.isRequired
 };
 
-export default MenuGroups;
+export default MenuCategories;
